f4ff792c7a9db0b72b939daf9bb562459e183f31
Veshialle <gruppimatteo@gmail.com>
2016-05-26 12:10:09 +0000

    Second Version
    Still a big issue, changed some function.
    (seems iI "read" another char (/n) from the fscanf function)

--- 243420.c
+++ 243420.c
@@ -14,77 +14,107 @@ Matricola: 243420
 
 int main(int argc, char **argv,char **envp)
 {
-	int status; 
-	pid_t pid;
-	char buffer[N]; /* Stringa in cui avrò l'intero comando dato dall'utente */
-	char kommand[N] = "/bin/";
-	char *token;
-	int i =0, k=0;
-	int nread;  /* saranno l'effettiva dimensione della stringa letta sulla mini-shell */
-	char *p;
-	const char space[2] = " ";
+	const char space[] = " ";;
 	while(1)
 	{
+		pid_t pid; /* Stringa in cui avrò l'intero comando dato dall'utente */
+		char buffer[N];
+		char *token;
+		char kommand[N] = "/bin/";
 		char s[2];
-		strcpy(s, "$:");
-		write(1, s, strlen(s));
-		nread = read(0, buffer, N);
-		if(nread<0)
+		char *p; // per le variabili d'ambiente
+		int nread;  /* saranno l'effettiva dimensione della stringa letta sulla mini-shell */
+		int status; 
+		//scanf("%s", buffer);
+		printf("$: ");
+		//nread = fgets(0, buffer, N);
+		// PROVARE CON FGETS
+		fgets(buffer, N, stdin);
+		if(buffer == NULL)
 		{
-			perror("Error reading command, please try again\n");
+			perror("Insert command, try again\n");
+			break;
+		}
+		printf("La stringa appena scritta: %s, grande %d \n", buffer, strlen(buffer));
+		char **ftok;
+		char *savepoint;
+		char *str1;
+		ftok = (char **)malloc(1*sizeof(char *));
+		int i=0, k;
+		for(i=0, str1 = buffer; ; i++, str1 = NULL)
+		{
+			token = strtok_r(str1, space, &savepoint);
+			if(token == NULL)
+				break;
+			ftok[i] = (char *)malloc(strlen(token)*sizeof(char));
+			ftok[i] = token;
+			printf("%s\n", token);
+			ftok = (char **)realloc(ftok, (i+1)*sizeof(char *));
+		}
+			/*
+
+			token = strtok_r(buffer, space, &savepoint);
+			ftok[i] = (char *)malloc(strlen(token)*sizeof(char));
+			ftok[i] = token;			
+			while(token != NULL)
+			{
+				token = strtok_r(NULL, space, &savepoint);
+				if((token == NULL))
+					break;
+				i++;
+				ftok = (char **)realloc(ftok, i*sizeof(char *));
+				ftok[i] = (char *)malloc(strlen(token)*sizeof(char));
+				strcpy(ftok[i], token);
+				write(1, ftok[i], strlen(ftok[i]));
+			}
+
+			if(i<1)
+			{
+				ftok = (char **)realloc(ftok, i*sizeof(char *));
+				ftok[i] = (char *)malloc(sizeof(char));
+				ftok[i] = '\0';
+			}
+			*/
+		if(!(strcmp(ftok[0], "exit")))
+		{
+			printf("Have a nice day, hope I could help you soon\n");
+			exit(0);
+		} 
+		else if (!(strcmp(ftok[0], "printenv")))
+		{
+			/* comando interno di stampa delle
+			variabili d'ambiente  */
+			while(p = *envp)
+			{
+				strcat(p, "\n");
+				write(1, p, strlen(p));
+				envp += 1;
+			}	
+		}
+		else  if((pid=fork())<0)
+		{
+			perror("Fork error, try again later!\n");
+			exit(-1);
+		}
+		else if(pid==0)
+		{
+			/* CODICE DEL FIGLIO
+			PER LA GESTIONE DEI COMANDI DA 
+			TERMINALE ESTERNI */
+			strncat(kommand, ftok[0], strlen(ftok[0]));
+			if((execv(kommand, ftok))==-1)
+			{
+				//perror("Command error, please check the syntax\n");
+				exit(-1);
+			}
 		}
 		else
 		{
-			char *ftok[N];
-			token = strtok(buffer, space);
-			i=0;
-			ftok[i] = token;
-			while(token != NULL)
-			{
-				token = strtok(NULL, space);
-				printf("%s\n", ftok[i]);
-				i++;
-				ftok[i] = token;
-			}
-			printf("%d\n", strlen(ftok[0]));
-			//printf("%s\n", ftok[0]);
-			/*
-			if(!(strcmp(ftok[0], "exit")))
-			{
-				printf("Have a nice day, hope I could help you soon\n");
-				exit(0);
-			} else if (!(strcmp(ftok[0], "printenv")))
-			{
-				/* comando interno di stampa delle
-				variabili d'ambiente  
-				while(p = *envp)
-				{
-					printf("%s\n", p);
-					envp += 1;
-				}	
-			}
-			else */ if((pid=fork())<0)
-			{
-				perror("Fork error, try again later!\n");
-				exit(-1);
-			}
-			else if(pid==0)
-			{
-				/* CODICE DEL FIGLIO
-				PER LA GESTIONE DEI COMANDI DA 
-				TERMINALE ESTERNI */
-				strncat(kommand, ftok[0], strlen(ftok[0]));
-				if((execv(kommand, ftok))==-1)
-				{
-					//perror("Command error, please check the syntax\n");
-					exit(-1);
-				}
-			}
-			
-			else
-			{
-				wait(&status);
-			}			
+			wait(&status);
 		}
+		for(k=0; k<i;k++)
+			free(ftok[i]);
+		free(ftok);
+		free(token);
 	}
 }
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 Assegnamento-SO-2016
+
+Testo dell'esercizio:
+Si realizzi un programma C che realizzi un min-shell con le seguenti caratteristiche:
+	1. Presenti all'utente un prompt, ad es. "$".
+	2. Recuperi dalla linea digitata dall'utente il comando e i suoi eventuali argomenti.
+	3. Sia in grado di gestire direttamente due comandi interni:
+		- printenv: visualizza tutte le variabili d'ambiente.
+		- exit: fa terminare lo shell.
+Si suggerisce l'utilizzo della funzione C strtok per estrarre comando ed argomenti dalla linea digitata dall'utente.
+
+Essendo un progetto pubblico è a disposizione di chiunque voglia trarne ispirazione, essendo un assegnamento universitario VALUTATO il copia incolla non è gradito affatto.
+Spero di essere stato di aiuto.
